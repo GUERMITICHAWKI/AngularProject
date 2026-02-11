c724392b0019d866529b75e999767e04743e8ca0
GUERMITICHAWKI <Guermitichawki@gmail.com>
2026-02-11 10:24:17 +0100

    feat :routage

--- projet1/src/app/app.routes.ts
+++ projet1/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { ListeFilmsComponent } from './liste-films/liste-films.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path : 'chemin1', component : Comp1Component},
+{path : 'chemin2', component : Comp2Component},
+{path: 'cinema', component: ListeFilmsComponent},
+];
