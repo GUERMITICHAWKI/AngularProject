4b882075b3fcb0073b821c5af866ce64f2623144
GUERMITICHAWKI <Guermitichawki@gmail.com>
2026-02-11 10:36:48 +0100

    feat: routing error

--- projet1/src/app/app.routes.ts
+++ projet1/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { ListeFilmsComponent } from './liste-films/liste-films.component';
+import { ErreurComponent } from './erreur/erreur.component';
 
 export const routes: Routes = [
 {path : 'chemin1', component : Comp1Component},
 {path : 'chemin2', component : Comp2Component},
 {path: 'cinema', component: ListeFilmsComponent},
+{path : '**', component : ErreurComponent},
 ];
